054ba1ce05e71b0efeb2df2b954f8cef75daa131
Daniel Lee <shaymin.android@gmail.com>
2019-04-07 12:22:26 +0800

    remove pods

--- Pods/Target Support Files/Pods-IG Direct/Pods-IG Direct-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IG_Direct : NSObject
-@end
-@implementation PodsDummy_Pods_IG_Direct
-@end
--- Pods/Target Support Files/Pods-IG DirectTests/Pods-IG DirectTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IG_DirectTests : NSObject
-@end
-@implementation PodsDummy_Pods_IG_DirectTests
-@end
