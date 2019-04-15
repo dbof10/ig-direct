6976d9fede4857a02074dfacecc18fd0701e23e5
Daniel Lee <shaymin.android@gmail.com>
2019-04-15 21:48:59 +0800

    split user router

--- igdirect-server/app.js
+++ igdirect-server/app.js
@@ -1,9 +1,7 @@
 import express from 'express';
 import {interval, throwError, defer, of} from 'rxjs';
 import {switchMap} from 'rxjs/operators';
-import {getDevice, getCookieStorage,clearCookieFiles, encodeBase64} from "./utils";
-
-const Client = require('instagram-private-api').V1;
+var usersRouter = require('./routes/users');
 
 const PORT = 5000;
 const POLLING_INTERVAL = 1000;
@@ -26,56 +24,7 @@ interval(POLLING_INTERVAL).pipe(
 // Set up the express app
 const app = express();
 app.use(express.json());
-
-// get all todos
-app.post('/api/v1/login', (req, res) => {
-
-    let userName = req.body.userName;
-    let password = req.body.password;
-    let encodedUserName = encodeBase64(userName);
-    defer(async function login() {
-        const device = getDevice(encodedUserName);
-        const storage = getCookieStorage(`${encodedUserName}.json`);
-        return  Client.Session.create(device, storage, userName, password)
-    })
-        .subscribe(
-            function () {
-                res.status(200).send({
-                    success: 'true'
-                })
-            },
-            function (err) {
-                res.status(401).send({
-                    error: err
-                })
-            }
-        );
-
-
-});
-
-app.post('/api/v1/logout', (req, res) => {
-
-    let userName = req.body.userName;
-    let encodedUserName = encodeBase64(userName);
-    defer(async function logout() {
-        return clearCookieFiles(`${encodedUserName}.json`);
-    })
-        .subscribe(
-            function (value) {
-                res.status(200).send({
-                    success: value
-                })
-            },
-            function (err) {
-                res.status(404).send({
-                    error: err
-                })
-            }
-        );
-
-
-});
+app.use('/users', usersRouter);
 
 const server = app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
--- igdirect-server/routes/users.js
+++ igdirect-server/routes/users.js
@@ -1,9 +1,57 @@
-var express = require('express');
-var router = express.Router();
+import {clearCookieFiles, encodeBase64, getCookieStorage, getDevice} from "../utils";
+import {defer} from "rxjs";
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const express = require('express');
+const router = express.Router();
+const Client = require('instagram-private-api').V1;
+
+router.post('/login', (req, res) => {
+
+  let userName = req.body.userName;
+  let password = req.body.password;
+  let encodedUserName = encodeBase64(userName);
+  defer(async function login() {
+    const device = getDevice(encodedUserName);
+    const storage = getCookieStorage(`${encodedUserName}.json`);
+    return Client.Session.create(device, storage, userName, password)
+  })
+      .subscribe(
+          function () {
+            res.status(200).send({
+              success: 'true'
+            })
+          },
+          function (err) {
+            res.status(401).send({
+              error: err
+            })
+          }
+      );
+
+
+});
+
+router.post('/logout', (req, res) => {
+
+  let userName = req.body.userName;
+  let encodedUserName = encodeBase64(userName);
+  defer(async function logout() {
+    return clearCookieFiles(`${encodedUserName}.json`);
+  })
+      .subscribe(
+          function (value) {
+            res.status(200).send({
+              success: value
+            })
+          },
+          function (err) {
+            res.status(404).send({
+              error: err
+            })
+          }
+      );
+
+
 });
 
 module.exports = router;
